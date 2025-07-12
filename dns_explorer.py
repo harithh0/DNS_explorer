db0ae5021f0ea6e0816b81a3254c284a21796372
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-07-11 22:02:01 -0500

    added dns requests

--- dns_explorer.py
+++ dns_explorer.py
@@ -1,3 +1,6 @@
+import socket
+from time import sleep
+
 import dns
 import dns.resolver
 
@@ -5,6 +8,50 @@ res = dns.resolver.Resolver()
 res.nameservers = ["8.8.8.8"]
 res.port = 53
 
-dns_result = res.resolve("google.com")
-for i in dns_result:
-    print(str(i))
+domain = "uca.edu"
+domains_and_results = {}
+
+with open("offensive_dns_exploration/dns_search.txt", "r") as f:
+    sub_domains = f.read().splitlines()
+
+# for i in dns_result:
+#     print(socket.gethostbyaddr(i.to_text())[0])
+
+
+def reverseDnsLookup(address: str) -> str:
+    """Returns hostname from IP address"""
+
+    # NOTE: can return alias aswell @ [1]
+    ptr_record = socket.gethostbyaddr(address)
+    return ptr_record[0]
+
+
+def dnsRequest(domain: str):
+    try:
+        dns_result = res.resolve(domain)
+        addresses_for_domain = [addr.to_text() for addr in dns_result]
+        print(f"{domain} {addresses_for_domain}")
+        for address in addresses_for_domain:
+            try:
+                rev_results = reverseDnsLookup(address)
+                print(rev_results)
+            except Exception as e:
+                print("no PTR record found for address:", address)
+                rev_results = []
+
+            print(f"sub for {address} -> {rev_results}")
+            if domain in domains_and_results:
+                domains_and_results[domain].append({address: rev_results})
+            else:
+                domains_and_results[domain] = [{address: rev_results}]
+    except Exception as e:
+        print(str(e))
+        pass
+        # print(f"{domain} couldn't find")
+
+
+for sub_domain in sub_domains:
+    created_req = f"{sub_domain}.{domain}"
+    dnsRequest(created_req)
+print("-" * 100)
+print(domains_and_results)
